0e98e05551e7d3497c4a7c95497a08fd63c85f58
im667 <koh9037@gmail.com>
2022-04-07 16:36:20 +0900

    ADD: Show pieChartView

--- CovidGraph-API/ViewController.swift
+++ CovidGraph-API/ViewController.swift
@@ -6,12 +6,112 @@
 //
 
 import UIKit
+import Charts
+import Alamofire
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var caseLabel: UILabel!
+    
+    @IBOutlet weak var newCaseLabel: UILabel!
+    
+    @IBOutlet weak var pieChartView: PieChartView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        self.fetchCovidOverview(completionHandler: {[weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result):
+                self.configurStackView(koreaCovidOverview: result.korea)
+                let covidOverviewList = self.makeCovidOverviewList(cityCovidOverview: result)
+                self.configurChartView(covidOverviewList: covidOverviewList)
+                
+            case let .failure(error):
+                debugPrint(error)
+            }
+        })
+        
+        
+    }
+
+
+    
+    func makeCovidOverviewList(
+        cityCovidOverview:CityCovidOverview
+    )->[CovidOverview] {
+        return [
+            cityCovidOverview.seoul,
+            cityCovidOverview.busan,
+            cityCovidOverview.incheon,
+            cityCovidOverview.daegu,
+            cityCovidOverview.sejong,
+            cityCovidOverview.chungnam,
+            cityCovidOverview.daejeon,
+            cityCovidOverview.chungbuk,
+            cityCovidOverview.gangwon,
+            cityCovidOverview.gyeonggi,
+            cityCovidOverview.ulsan,
+            cityCovidOverview.gwangju,
+            cityCovidOverview.jeonnam,
+            cityCovidOverview.jeonbuk,
+            cityCovidOverview.gyeongbuk,
+            cityCovidOverview.gyeongnam,
+            cityCovidOverview.jeju
+            
+        ]
+    }
+    
+    func configurChartView(covidOverviewList: [CovidOverview]){
+        let entrise = covidOverviewList.compactMap { [weak self] overView -> PieChartDataEntry? in
+            guard let self = self else { return nil }
+            return PieChartDataEntry(
+                value: self.removeFormatString(string: overView.newCase),
+                label: overView.countryName,
+                data: overView
+            )
+        }
+        let dataSet = PieChartDataSet(entries: entrise, label: "코로나 발생 현황")
+        self.pieChartView.data = PieChartData(dataSet: dataSet)
+    }
+    
+    func removeFormatString(string: String)-> Double {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter.number(from: string)?.doubleValue ?? 0
+    }
+    
+    func configurStackView(koreaCovidOverview: CovidOverview) {
+        self.caseLabel.text = "\(koreaCovidOverview.totalCase)명"
+        self.newCaseLabel.text = "\(koreaCovidOverview.newCase)명"
+    }
+    
+    
+    func fetchCovidOverview(
+        completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+    ){
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = Key.param
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: {response in
+                switch response.result {
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                    
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+            })
     }
 
 
---
+++ Pods/Target Support Files/Pods-CovidGraph-API/Pods-CovidGraph-API-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CovidGraph_API : NSObject
+@end
+@implementation PodsDummy_Pods_CovidGraph_API
+@end
